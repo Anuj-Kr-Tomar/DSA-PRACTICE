83ab0924e4f8fea6180818fcf9593aa37e90a9e4
anuj <anuj@gmail.com>
2025-08-24 00:11:59 +0530

    Added recursion folder with fibo.java

---
+++ src/Recursion/fibonacci.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibo(5));
+    }
+    static int fibo(int n){
+        if(n < 2){
+            return 1;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
